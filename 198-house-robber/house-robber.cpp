986dd0e1e1dfcb4590110de4c64647fa2c0fb51a
Dharani-M1 <dharanimagesh1817@gmail.com>
2025-05-19 22:03:53 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (89.97%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -5,8 +5,8 @@ public:
         if (n == 0) return 0;
         if (n == 1) return nums[0];
         
-        int prev2 = nums[0]; // dp[i-2]
-        int prev1 = max(nums[0], nums[1]); // dp[i-1]
+        int prev2 = nums[0]; 
+        int prev1 = max(nums[0], nums[1]);
         
         for (int i = 2; i < n; ++i) {
             int current = max(prev1, nums[i] + prev2);
