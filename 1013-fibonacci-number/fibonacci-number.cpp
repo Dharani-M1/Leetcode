5f1431ae75a8ab23058afa45dace0a61449042e9
Dharani-M1 <dharanimagesh1817@gmail.com>
2025-05-22 22:26:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.57%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        int prev=0,curr=1,next;
+        if(n==0)return NULL;
+        if(n==1)return n;
+
+        for(int i=1;i<n;i++){
+            next=prev+curr;
+            prev=curr;
+            curr=next;
+        }
+        return next;
+    }
+};
