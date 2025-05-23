4f4223d87ef2dbf70c1657a4f4fee563f585ed0e
Dharani-M1 <dharanimagesh1817@gmail.com>
2025-05-23 17:46:27 +0530

    Time: 4 ms (56.56%) | Memory: 19.9 MB (45.43%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     static bool compare(const vector<int>& a, const vector<int>& b) {
-    return a[1] > b[1];  
+        return a[1] > b[1];
     }
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-    int units = 0;
-    sort(boxTypes.begin(), boxTypes.end(), compare); 
-    for (int i = 0; i < boxTypes.size(); i++) {
-    int load = min(boxTypes[i][0], truckSize);  
-    units += load * boxTypes[i][1];
-    truckSize -= load;
-    if (truckSize == 0) break;
-    }
-    return units;
+        int units = 0;
+        sort(boxTypes.begin(), boxTypes.end(), compare);
+        for (int i = 0; i < boxTypes.size(); i++) {
+            int load = min(boxTypes[i][0], truckSize);
+            units += load * boxTypes[i][1];
+            truckSize -= load;
+            if (truckSize == 0)
+                break;
+        }
+        return units;
     }
 };
