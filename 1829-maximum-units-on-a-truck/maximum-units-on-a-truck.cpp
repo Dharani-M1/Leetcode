69c634f4408a490ec8f3c9bb2b5b13735a03ce47
Dharani-M1 <dharanimagesh1817@gmail.com>
2025-05-23 17:46:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (66.35%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
@@ -4,14 +4,14 @@ public:
     return a[1] > b[1];  
     }
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        int units = 0;
-        sort(boxTypes.begin(), boxTypes.end(), compare); 
-        for (int i = 0; i < boxTypes.size(); i++) {
-        int load = min(boxTypes[i][0], truckSize);  
-        units += load * boxTypes[i][1];
-        truckSize -= load;
-        if (truckSize == 0) break;
-        }
-        return units;
+    int units = 0;
+    sort(boxTypes.begin(), boxTypes.end(), compare); 
+    for (int i = 0; i < boxTypes.size(); i++) {
+    int load = min(boxTypes[i][0], truckSize);  
+    units += load * boxTypes[i][1];
+    truckSize -= load;
+    if (truckSize == 0) break;
+    }
+    return units;
     }
 };
