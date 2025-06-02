52f2c446c0908a3a741b503f0e13c96a93ef5df1
Dharani-M1 <dharanimagesh1817@gmail.com>
2025-06-02 11:28:14 +0530

    Time: 0 ms (100.00%) | Memory: 100.4 MB (64.85%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -3,7 +3,7 @@ public:
     bool isMonotonic(vector<int>& nums) {
         bool increasing = true;
         bool decreasing = true;
-        for (int i = 1; i < nums.size(); ++i) {
+        for (int i = 1; i < nums.size(); i++) {
             if (nums[i] > nums[i - 1]) {
             decreasing = false;
             }
