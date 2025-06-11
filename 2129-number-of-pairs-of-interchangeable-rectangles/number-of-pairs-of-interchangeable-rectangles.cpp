b09e6e31cad06c379624f98f5b30c9275e177359
Dharani-M1 <dharanimagesh1817@gmail.com>
2025-06-11 11:07:51 +0530

    Time: 87 ms (81.90%) | Memory: 145.8 MB (72.14%) - LeetSync

--- 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.cpp
+++ 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.cpp
@@ -3,13 +3,11 @@ public:
     long long interchangeableRectangles(vector<vector<int>>& rectangles) {
         unordered_map<double, long long> ratioCount;
         long long ans = 0;
-
         for (auto& rect : rectangles) {
             double ratio = (double)rect[0] / rect[1];
             ans += ratioCount[ratio];
             ratioCount[ratio]++;
         }
-
         return ans;
     }
 };
