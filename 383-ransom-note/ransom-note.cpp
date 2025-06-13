60e0a2aaba548ea114528913e26419bee06fd424
Dharani-M1 <dharanimagesh1817@gmail.com>
2025-06-13 10:25:05 +0530

    Time: 7 ms (31.54%) | Memory: 12 MB (10.21%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> maga;
+        for (char ch : magazine) {
+            maga[ch]++;
+        }
+        for (char ch : ransomNote) {
+            if (maga[ch] <= 0) {
+                return false;
+            }
+            maga[ch]--;
+        }
+        return true;
+    }
+};
